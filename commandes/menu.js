05a21961bdb1f708a16c3ebe69ee23307799d2f7
Malvin King <malvink003@gmail.com>
2024-09-25 19:14:33 +0200

    Add files via upload

---
+++ commandes/menu.js
@@ -0,0 +1,184 @@
+const util = require('util');
+
+const fs = require('fs-extra');
+
+const { zokou } = require(__dirname + "/../framework/zokou");
+
+const { format } = require(__dirname + "/../framework/mesfonctions");
+
+const os = require("os");
+
+const moment = require("moment-timezone");
+
+const s = require(__dirname + "/../set");
+
+
+
+zokou({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+
+    let { cm } = require(__dirname + "/../framework//zokou");
+
+    var coms = {};
+
+    var mode = "public";
+
+    
+
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+
+        mode = "private";
+
+    }
+
+
+
+
+
+    
+
+
+
+    cm.map(async (com, index) => {
+
+        if (!coms[com.categorie])
+
+            coms[com.categorie] = [];
+
+        coms[com.categorie].push(com.nomCom);
+
+    });
+
+
+
+    moment.tz.setDefault(s.TZ);
+
+
+
+// Créer une date et une heure en GMT
+
+const temps = moment().format('HH:mm:ss');
+
+const date = moment().format('DD/MM/YYYY');
+
+
+
+  let infoMsg =  `
+
+┏━━ 𝗥𝗔𝗩𝗘𝗡𝗦 𝗠𝗗 ━━┓
+┃   Mode: ${mode}
+┃   User : ${s.OWNER_NAME}
+┃
+┣━🔥ravens md new vision 𒈒━➠
+┗━━━𒈒by Malvin King𒈒━━┛\n\n`;
+
+
+    
+
+let menuMsg = `
+┏━━━━━━━━━━━━━━┓
+┣🌀ravens md  
+┣🌀by Malvin Tech
+┗━━━━━━━━━━━━━━┛\n
+
+
+
+
+ravens md cmds𒈒
+`;
+
+
+
+    for (const cat in coms) {
+
+        menuMsg += `┏━━━━━⚼ ${cat}`;
+
+        for (const cmd of coms[cat]) {
+
+            menuMsg += `
+┃༆ ${cmd}`;
+
+        }
+
+        menuMsg += `
+┗━━━━━━━━━━━━━━┛\n`
+
+    }
+
+
+
+    menuMsg += `
+
+
+︎┏━━━━━━━━━━━━━━┓
+️┣❏🌀ravens md bot 
+┣❏🔥by Malvin King 
+┗━━━━━━━━━━━━━━┛\n
+
+
+┏━━━━━━━━━━━━━━┓
+┃🥰powered by Malvin Tech 
+┗━━━━━━━━━━━━━━┛\n
+
+
+`;
+
+
+
+   var lien = mybotpic();
+
+
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+
+    try {
+
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *RAVENS-MD*, déveloper MALVIN" , gifPlayback : true }, { quoted: ms });
+
+    }
+
+    catch (e) {
+
+        console.log("🥵🥵 Menu error " + e);
+
+        repondre("🥵🥵 Menu error " + e);
+
+    }
+
+} 
+
+// Vérification pour .jpeg ou .png
+
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+
+    try {
+
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *RAVENS-bot*, déveloper kingmalvn" }, { quoted: ms });
+
+    }
+
+    catch (e) {
+
+        console.log("🥵🥵 Menu error " + e);
+
+        repondre("🥵🥵 Menu error " + e);
+
+    }
+
+} 
+
+else {
+
+    
+
+    repondre(infoMsg + menuMsg);
+
+    
+
+}
+
+
+
+});
+          
