06d820567a39ab3f16b2c647a03ea277b2f77a3c
Malvin King <malvink003@gmail.com>
2024-09-30 23:34:11 +0200

    Update crick.js

--- src/plugin/crick.js
+++ src/plugin/crick.js
@@ -28,7 +28,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
       const result = response.data;
 
       let formattedResult = `╭══════════════•∞•══╮\n`;
-      formattedResult += `│⿻   *𝙹𝙾𝚎𝚕*\n`;
+      formattedResult += `│⿻   *𝙻𝚘𝚛𝚍*\n`;
       formattedResult += `│⿻   *LIVE MATCH INFO* ✨\n`;
       formattedResult += `│⿻\n`;
 
