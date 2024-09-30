2baeb9cdcd34e8657a4871f390bcebe393309c3c
Malvin King <malvink003@gmail.com>
2024-09-30 22:50:56 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&dVnLFSpN",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&m7LR9uYY",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
@@ -19,8 +19,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "𝙹𝙾𝚎𝚕",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "255714595078",
+  OWNER_NAME: process.env.OWNER_NAME || "ʟᴏʀᴅ ᴍᴀʟᴠɪɴ",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "263780166288",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
