f2e929f7e27d1dd5384e6eb8f8e46f5b43d700a2
Malvin King <malvink003@gmail.com>
2024-09-30 23:43:06 +0200

    Update gpt.js

--- src/plugin/gpt.js
+++ src/plugin/gpt.js
@@ -117,7 +117,7 @@ const prompt = m.body.slice(prefix.length + cmd.length).trim();
                                     text: answer
                                 }),
                                 footer: proto.Message.InteractiveMessage.Footer.create({
-                                    text: "> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙹𝙾𝚎𝚕"
+                                    text: "> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙻𝚘𝚛𝚍"
                                 }),
                                 header: proto.Message.InteractiveMessage.Header.create({
                                     title: "",
