a4dbfc14f4d573b184f4cfecdef187854c4ac863
Malvin King <malvink003@gmail.com>
2024-10-07 15:57:24 +0200

    Add files via upload

---
+++ src/plugin/ping.js
@@ -0,0 +1,19 @@
+import config from '../../config.cjs';
+
+const ping = async (m, sock) => {
+  const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
+
+  if (cmd === "ping") {
+    const start = new Date().getTime();
+    await m.React('⚡');
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+    const text = `*_🔥⃝𝙻𝙾𝚁𝙳 𝙼𝙳 вσт ѕρєє∂: ${responseTime.toFixed(2)} s_*`;
+    sock.sendMessage(m.from, { text }, { quoted: m });
+  }
+}
+
+export default ping;
